5a486da4081bb00fc309258ea8bf342233043fc4
Allen <28549880+M0rtale@users.noreply.github.com>
2019-08-12 15:16:06 -0400

    At least compiles now, still very buggy
    wrong impl of sqrt, changed up a little.

--- QQSGHuaRongDao/QQSGHuaRongDao/main.c
+++ QQSGHuaRongDao/QQSGHuaRongDao/main.c
@@ -4,38 +4,61 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <math.h>
 #include <assert.h>
+#include <math.h>
 
 #include "lib/queue.h"
 #include "lib/hdict.h"
 #include "lib/xalloc.h"
 #include "board.h"
 
-int sqrt(int x, int y)
+int sqrt_i(int x)
 //@requires y >= 0;
 //@ensures POW(x, y) == \result;
 {
-	int b = x; /* Line 19 */
-	int e = y; /* Line 20 */
-	int r = 1; /* Line 21 */
-	while (e > 0) /* Line 22 */
-	//@loop_invariant e >= 0; /* Line 23 */
-	//@loop_invariant POW(b, e) * r == POW(x, y);
-	{
-		if (e % 2 == 1) { /* Line 26 */
-			r = b * r;      /* Line 27 */
-		}
-		b = b * b;        /* Line 29 */
-		e = e / 2;        /* Line 30 */
+	float sqrt = 0;
+	float temp = 0;
+
+	// store the half of the given number e.g from 256 => 128
+	sqrt = (float)x / 2;
+	temp = 0;
+
+	// Iterate until sqrt is different of temp, that is updated on the loop
+	while (sqrt != temp) {
+		// initially 0, is updated with the initial value of 128
+		// (on second iteration = 65)
+		// and so on
+		temp = sqrt;
+
+		// Then, replace values (256 / 128 + 128 ) / 2 = 65
+		// (on second iteration 34.46923076923077)
+		// and so on
+		sqrt = (x / temp + temp) / 2;
 	}
-	return r;
+	return (int)sqrt;
+}
+
+void swap(uint32_t *one, uint32_t *two)
+{
+	uint32_t temp = *one;
+	*one = *two;
+	*two = temp;
+}
+
+void printBoard(board_t *b)
+{
+	printf("\n");
+
+	for (int i = 0; i < sqrt_i(b->board->size); i++)
+	{
+
+	} 
 }
 
 //returns 4 board, each indicating whether moves up down right or left
 board_t **get_move(board_t *in)
 {
-	size_t row = sqrt(in->board->size, 2);
+	size_t row = sqrt_i(in->board->size);
 	board_t **arr = xmalloc(sizeof(board_t *) * 4);
 
 	//create new array
@@ -78,21 +101,45 @@ board_t **get_move(board_t *in)
 	*down = 3;
 
 	//get position of empty
-	uint32_t empty = -1;
-	for (uint32_t i; i < arr[0]->board->size; i++)
+	uint32_t empty = (uint32_t)-1;
+	for (uint32_t i = 0; i < arr[0]->board->size; i++)
 	{
+		//printf("%d", arr[0]->board->arr[i]);
 		if (arr[0]->board->arr[i] == 0)
 		{
 			empty = i;
 			break;
 		}
 	}
-	assert(empty == -1); // you fucked up.
+	//printf("\n%d\n", empty);
+	assert(empty != (uint32_t)-1); // you fucked up.
 
 	//if not left most
+	// TODO: fix this shit, is it 0 or 1.
+	if (empty % row - 1 > 0)
+	{
+		swap(&arr[0]->board->arr[empty], &arr[0]->board->arr[empty - 1]);
+		enq(arr[0]->path, left);
+	}
 	//if not up most
+	if (empty / row - 1 > 0)
+	{
+		swap(&arr[1]->board->arr[empty], &arr[1]->board->arr[empty - row]);
+		enq(arr[1]->path, up);
+	}
 	//if not right most
+	if(empty % row + 1 <= row)
+	{
+		swap(&arr[2]->board->arr[empty], &arr[2]->board->arr[empty + 1]);
+		enq(arr[2]->path, right);
+	}
 	//if not down most
+	if (empty / row + 1 <= row)
+	{
+		swap(&arr[3]->board->arr[empty], &arr[3]->board->arr[empty + row]);
+		enq(arr[3]->path, down);
+	}
+	return arr;
 }
 //0 - left
 //1 - up
@@ -100,17 +147,69 @@ board_t **get_move(board_t *in)
 //3 - down
 
 //classic BFS search
-bool solve(size_t size, board_t *initial, board_t *target)
+bool solve(board_t *initial, board_t *target)
 {
 	//we are done if it is already equal
-	if (arr_equal(initial->board->arr, target->board->arr)) return true;
+	if (arr_equal(initial->board, target->board)) return true;
 
 	queue_t Q = queue_new();
 	enq(Q, initial);
+
+	hdict_t dict = ht_new(initial->board->size);
+	hdict_insert(dict, initial);
 	while (!queue_empty(Q))
 	{
 		board_t *cur = deq(Q);
+		board_t **move = get_move(cur);
+
+
+		for (int i = 0; i < 4; i++)
+		{
+			//if success in reproducing the path
+			if (arr_equal(move[i]->board, target->board))
+			{
+				queue_t path = move[i]->path;
+				while (!queue_empty(path))
+				{
+					uint32_t paths = *(uint32_t *)deq(path);
+					switch (paths)
+					{
+					case 0: // left
+					{
+						printf("l ");
+						break;
+					}
+					case 1: //up
+					{
+						printf("u ");
+						break;
+					}
+					case 2: //right
+					{
+						printf("r ");
+						break;
+					}
+					case 3: //down
+					{
+						printf("d ");
+						break;
+					}
+					}
+				}
+				printf("\n");
+				
+				return true;
+			}
+			//if the thing is bad
+			if (ht_lookup(dict, move[i]->board) == NULL)
+			{
+				enq(Q, move[i]);
+				hdict_insert(dict, move[i]);
+			}
+		}
 	}
+	//no path could be found.
+	return false;
 }
 
 int main(int argc, char** argv)
@@ -126,24 +225,38 @@ int main(int argc, char** argv)
 	initial->board = xmalloc(sizeof(IntArray_t));
 	initial->board->arr = xmalloc(sizeof(uint32_t) * size);
 	initial->board->size = size;
+	initial->path = queue_new();
 
-	for (int i = 2; i < size + 2; i++)
+	for (size_t i = 2; i < size + 2; i++)
 	{
 		initial->board->arr[i - 2] = atoi(argv[i]);
+		printf("%d ", atoi(argv[i]));
 	}
+
+	printf("\n");
 
 	board_t *target = xmalloc(sizeof(board_t));
 	target->board = xmalloc(sizeof(IntArray_t));
 	target->board->arr = xmalloc(sizeof(uint32_t) * size);
 	target->board->size = size;
 
-	size += size; // skip initial
-
-	for (int i = size + 2; i < size + 2; i++)
+	for (size_t i = size + 2; i < size + size + 2; i++)
 	{
 		target->board->arr[i - (size + 2)] = atoi(argv[i]);
+		printf("%d ", atoi(argv[i]));
 	}
 
+	printf("\n");
+
 	//TODO: fix leak
-	return solve(size/2, initial, target);
+	if (solve(initial, target))
+	{
+		printf("Success!\n");
+		return 0;
+	}
+	else
+	{
+		printf("err...\n");
+		return 1;
+	}
 }
